99d648d0022739b312d846aebc21776b7d8a67ba
srisabarikkumar@gmail.com <srisabarikkumar@gmail.com>
2024-10-09 10:09:24 +0530

    new change

--- src/components/LatestJobCards.jsx
+++ src/components/LatestJobCards.jsx
@@ -7,7 +7,7 @@ const LatestJobCards = ({ job }) => {
   return (
     <div
       onClick={() => navigate(`/description/${job._id}`)}
-      className="p-5 rounded-md shadow-xl w-96 bg-white border border-gray-100 cursor-pointer"
+      className="p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer"
     >
       <div>
         <h1 className="font-medium text-lg">{job?.company?.name}</h1>
@@ -17,7 +17,7 @@ const LatestJobCards = ({ job }) => {
         <h1 className="font-bold text-lg my-2">{job?.title}</h1>
         <p className="text-sm text-gray-600">{job?.description}</p>
       </div>
-      <div className="flex lg:flex-row md:flex-row sm:flex-col items-center gap-2 mt-4">
+      <div className="flex gap-2 mt-4">
         <Badge className={"text-blue-700 font-bold"} variant="ghost">
           {job?.position} Positions
         </Badge>
